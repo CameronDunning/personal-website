d9bb2a77fd64fa9a34bc40d44307e11a3fff1b48
Cameron <cdunning@avalanche.ca>
2022-07-15 19:28:48 -0700

    Initial Ant Design Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'antd/dist/antd.css'
+import './index.css'
+import './App.css'
+
+import { Breadcrumb, Layout, Menu } from 'antd'
+const { Header, Content, Footer } = Layout
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const menuItems = [
+        { key: '1', label: 'Home' },
+        { key: '2', label: 'Professional' },
+        { key: '3', label: 'Personal' },
+        { key: '4', label: 'Contact' },
+    ]
+
+    return (
+        <Layout className="layout" style={{ minHeight: '100vh' }}>
+            <Header>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} items={menuItems} />
+            </Header>
+            <Content>
+                <div className="site-layout-content">Content</div>
+            </Content>
+            <Footer>©2022 Created by Cameron Dunning</Footer>
+        </Layout>
+    )
 }
 
-export default App;
+export default App
